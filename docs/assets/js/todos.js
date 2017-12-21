e011ddb9f66e1ce0462f47bf2ee58647ecf55a91
jaczylianhel <jaczylianhel@gmail.com>
2017-12-21 13:30:57 +0100

    todos

---
+++ docs/assets/js/todos.js
@@ -0,0 +1,24 @@
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+//remove from list
+
+$("ul").on("click", "span", function (e) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (e) {
+    if (e.which === 13) {
+        var todo = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i> </span>" + todo + "</li>");
+    }
+});
+
+$(".fa-plus").click(function () {
+    $("input[type='text']").fadeToggle();
+});
